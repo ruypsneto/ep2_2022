b364683ee2030c75f5bf32655206a80c92550ed6
ruyneto <ruypsn@gmail.com>
2022-04-27 16:36:46 -0300

    adicionei a função sorteia_pais e a esta_na_lista

--- funcoes.py
+++ funcoes.py
@@ -1,7 +1,7 @@
 # aqui vamo colocar as funções pra puxar tudo de um import soh
-#  deixei a função math como m pra ser mais facil 
-import math as m 
-def haversine(r, y1, k1, y2, k2):
+import math as m          #  deixei a função math como m pra ser mais facil 
+import random
+def haversine(r, y1, k1, y2, k2):      # essa função calcula a distancia 
     # y1 e y2  = y cursiva estranho
     # k1 e k2 = y de ponta cabeça
     y1 = m.radians(y1)
@@ -14,3 +14,20 @@ def haversine(r, y1, k1, y2, k2):
     return d 
     # função ta certa e funcionando
 
+
+
+
+def esta_na_lista(pais, lista):        #essa função verifica se determinado elemento esta em uma lista 
+    for px in lista:
+        if pais in px[0]:
+            return True 
+    return False
+    # função ta certa e funcionando
+
+
+def sorteia_pais(dici):         #função que sorteia um elemento de dicionario  
+    lp = []
+    for p in dici.keys():
+        lp.append(p)
+    return random.choice(lp)
+    # função ta certa e funcionando
