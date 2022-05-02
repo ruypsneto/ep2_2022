ccae9a199e022bb5d98725aee70ff4db9665f721
ruyneto <ruypsn@gmail.com>
2022-05-02 12:58:27 -0300

    esqueleto do jogo

--- principal.py
+++ principal.py
@@ -2,3 +2,23 @@
 import funcoes as f 
 from base_de_dados import dados
 from base_de_dados import raio_terra
+
+quer_jogar_novamente = True
+jogo = True 
+dados_convertidos = f.normaliza(dados)
+
+while quer_jogar_novamente == True:         # aqui vai deixar o jogo em loop ate que o jogador nao queira mais 
+    while jogo == True:        # aqui vai ser onde roda o jogo 
+        # estrutura a pensar, tem um break pra n ferrar com o codigo mas depois tira 
+            break 
+
+
+    quer_jogar = str(input('Quer jogar novamente?[sim/nao]  '))
+    while quer_jogar != 'sim' and quer_jogar != 'nao':
+        print('invalido') 
+        quer_jogar = str(input('Quer jogar novamente?[sim/nao]  '))
+    if quer_jogar == 'sim':
+            jogo == True 
+    else:
+        print('Obrigado por jogar!!!')
+        quer_jogar_novamente = False
